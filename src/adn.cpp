db693eadd2b465f1b1cb795696ab3c9606689d98
ShadoWing1 <oyungemer14@gmail.com>
2023-03-14 18:55:14 +0300

    .

--- src/adn.cpp
+++ src/adn.cpp
@@ -3,6 +3,10 @@
 #include <thread>
 #include <chrono>
 
+#define Big_A 65
+
+#define Small_A 97
+
 Yazilacak::Yazilacak(std::string girdi)
 {
     for(const auto& d : girdi)
@@ -19,7 +23,7 @@ void Yazilacak::yazma()
 
         int R = deneme;
 
-        if(R < 91)
+        if(R > 64 && R < 91)
         {
             for(int i = 65;i <= deneme; i++)
             {
@@ -30,7 +34,7 @@ void Yazilacak::yazma()
             kelime += u;
         }
 
-        else if(R > 91)
+        else if(R > 96 && R <123)
         {
             for(int i = 97; i<=deneme; i++)
             {
@@ -41,6 +45,43 @@ void Yazilacak::yazma()
             kelime += u;
         }
 
+        else if(R > 47 && R <58)
+        {
+            for(int i = 48; i<=deneme; i++)
+            {
+                u = i;
+                std::cout << kelime << u << "\n";
+                std::this_thread::sleep_for(std::chrono::duration<double, std::milli>(100));
+            }
+            kelime += u;
+        }
+
+        else if(R > 32 && R <48)
+        {
+            for(int i = 33; i<=deneme; i++)
+            {
+                u = i;
+                std::cout << kelime << u << "\n";
+                std::this_thread::sleep_for(std::chrono::duration<double, std::milli>(100));
+            }
+            kelime += u;
+        }
+
+        else if(R > 90 && R <97)
+        {
+            for(int i = 91; i<=deneme; i++)
+            {
+                u = i;
+                std::cout << kelime << u << "\n";
+                std::this_thread::sleep_for(std::chrono::duration<double, std::milli>(100));
+            }
+            kelime += u;
+        }
+        
+        else if(R > 122)
+        {
+            std::cout << "Desteklemiyor" << "\n"; // maybe one day but dont now
+        }
         if(R == 32)
         {
             u = 32;
